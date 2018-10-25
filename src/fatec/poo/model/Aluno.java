37add17cd31cac4a6feca489b991321963e3fa0a
SABRINA LUCIANA VIEIRA PINTO <0030481613035@lti.fatecso.edu.br>
2018-10-25 19:55:05 -0300

    Criação das Classes Aluno e Instrutor

---
+++ src/fatec/poo/model/Aluno.java
@@ -0,0 +1,13 @@
+
+package fatec.poo.model;
+
+/* @author 0030481613035 */
+public class Aluno {
+    private String escolaridade;
+
+    public void setEscolaridade(String escolaridade) {
+        this.escolaridade = escolaridade;
+    }
+    
+    
+}
