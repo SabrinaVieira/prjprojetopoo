fadea73e0dee0ceaa883cdcb14011dbbb061152d
SABRINA LUCIANA VIEIRA PINTO <0030481613035@lti.fatecso.edu.br>
2018-10-25 20:08:43 -0300

    Alteracao das Classes Aluno e Instrutor

--- src/fatec/poo/model/Aluno.java
+++ src/fatec/poo/model/Aluno.java
@@ -2,9 +2,13 @@
 package fatec.poo.model;
 
 /* @author 0030481613035 */
-public class Aluno {
+public class Aluno extends Pessoa{
     private String escolaridade;
 
+    public Aluno(String cpf, String nome) {
+        super(cpf, nome);
+    }
+    
     public void setEscolaridade(String escolaridade) {
         this.escolaridade = escolaridade;
     }
