0b8634e99a5a05b5c7585cf5f63885032059adf9
Gabriel Moraes <gabriel.moraes@melhorescola.net>
2018-11-18 16:42:54 -0200

    funcao para validar cpf

--- src/fatec/poo/model/Pessoa.java
+++ src/fatec/poo/model/Pessoa.java
@@ -91,6 +91,61 @@ public class Pessoa {
         this.email = email;
     }
     
+    public static boolean validarCPF(String cpf){
+        if(!validaPrimeiroDigito(cpf))
+            return false;
+        if(!validaSegundoDigito(cpf))
+            return false;
+
+        return true;
+    }
     
+    /**
+     * Metodo para validar primeiro digito
+     * @param cpf 
+     * @return 
+     */
+    public static boolean validaPrimeiroDigito(String cpf){
+        int somaNumeros = 0;
+        int countCol;
+        
+        for(countCol = 1; countCol < 10; countCol++){
+            int number = Integer.parseInt(cpf.substring( countCol - 1, countCol));
+            somaNumeros += number * countCol;
+        }
+        int numValidador = Integer.parseInt(cpf.substring(9,10));
+        int divisao = somaNumeros % 11;
+        if(divisao == numValidador || (divisao == 10 && numValidador == 0)){
+            return true;
+        }
+        
+        return false;
+    }
+    
+    
+    /**
+     * Metodo para validar segudo digito
+     * @param cpf 
+     * @return 
+     */
+    public static boolean validaSegundoDigito(String cpf){
+        
+        int somaNumeros = 0;
+        int countCol = 0;
+        
+        for(int i = 11; i > 1; i--){
+            int number = Integer.parseInt(cpf.substring( countCol, countCol + 1));
+            somaNumeros += number * i;
+            countCol++;
+        }
+        int numValidador = Integer.parseInt(cpf.substring(10,11));
+        int validador = (somaNumeros * 10) % 11;
+
+        if(validador == numValidador || (validador == 10 && numValidador == 0)){
+            return true;
+        }
+        
+        return false;
+    }
     
 }
